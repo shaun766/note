5d2fdfa6e492c904baf8471a294eff5ee5e6d4d4
shaun76 <shaunreegans76@gmail.com>
2025-07-05 12:42:23 +0530

    before lucid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
+import Header from "@/mycomponents/Header";
 export default function Home() {
   return (
-    <div className="w-full h-screen pr-8 flex flex-col overflow-hidden">
-      {/* Header (auto height) */}
-      <div className="flex w-full p-1 justify-between">
-        <div></div>
-        <p className="text-3xl">Title</p>
-        <Button className="bg-background text-foreground hover:bg-background border-1">
-          update
-        </Button>
-      </div>
+    <div  className="w-full h-screen pr-8 flex flex-col gap-2 overflow-hidden">
+    <Header />
 
       {/* Remaining space */}
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Card className="flex-1 p-0 overflow-hidden">
+      <div className="fw-full h-full pr-8 flex flex-col overflow-hidden">
+        <Card className="flex-1 p-0 overflow-hidden h-full">
           <Textarea className="h-full w-full resize-none overflow-y-auto" placeholder="start typing..."/>
         </Card>
       </div>
---
+++ src/mycomponents/Header.tsx
@@ -0,0 +1,26 @@
+"use client"
+// import 
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
+function title(){
+    const[editTitle, setEditTitle]=useState(false)
+}
+const Header = () => {
+  return (
+      <div className="w-full pr-8 flex flex-col overflow-hidden">
+      {/* Header (auto height) */}
+      <div className="flex w-full p-1 justify-between">
+        <div></div>
+        <div className="flex justify-around"> 
+            <h1 className="text-3xl ">Title</h1>
+            <Button className=''>o</Button></div>
+        <Button className="bg-background text-foreground hover:bg-background border-1 ">
+          update
+        </Button>
+    </div>
+    </div>
+  )
+}
+
+export default Header
